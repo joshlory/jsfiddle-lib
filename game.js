bbb0c3b89d0969fe6498128fdcebfe3cd67c572b
Josh Lory <loryjn@missouri.edu>
2013-09-25 22:33:30 -0700

    Update game.js

--- game.js
+++ game.js
@@ -23,5 +23,21 @@ Object.defineProperties(player, {
         set : function(value) {
             ball.style.left = value + "px";
         }
+    },
+    bottom : {
+        get : function() {
+            return parseFloat(ball.style.top) + ball.offsetHeight || ball.offsetHeight;
+        },
+        set : function(value) {
+            ball.style.top = value - ball.offsetHeight + "px";
+        }
+    },
+    right : {
+        get : function() {
+            return parseFloat(ball.style.left) + ball.offsetWidth || ball.offsetWidth;
+        },
+        set : function(value) {
+            ball.style.left = value - ball.offsetWidth + "px";
+        }
     }
 });
