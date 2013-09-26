f95fa6dbb34342144758702c5b894d2cc4f9e831
Josh Lory <loryjn@missouri.edu>
2013-09-25 21:55:53 -0700

    Added keys event listeners

--- game.js
+++ game.js
@@ -1,3 +1,11 @@
+var keys = {};
+window.addEventListener("keydown", function(e) {
+    keys[e.keyCode] = true;
+});
+window.addEventListener("keyup", function(e) {
+    keys[e.keyCode] = false;  
+});
+
 var player = {};
 Object.defineProperties(player, {
     top : {
