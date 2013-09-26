5c80b04ea5f3f37e2a80b24a860a7c4f2b947bc4
Josh Lory <loryjn@missouri.edu>
2013-09-25 23:55:03 -0700

    Fix platform code

--- game.js
+++ game.js
@@ -6,48 +6,52 @@ window.addEventListener("keyup", function(e) {
     keys[e.keyCode] = false;  
 });
 
+function Sprite(element) {
+    this.element = element;
+}
+Object.defineProperties(Sprite.prototype, {
+    top : {
+        get : function() {
+            return parseFloat(this.element.style.top) || 0;
+        },
+        set : function(value) {
+            this.element.style.top = value + "px";
+        }
+    },
+    left : {
+        get : function() {
+            return parseFloat(this.element.style.left) || 0;
+        },
+        set : function(value) {
+            this.element.style.left = value + "px";
+        }
+    },
+    bottom : {
+        get : function() {
+            return parseFloat(this.element.style.top) + this.element.offsetHeight || this.element.offsetHeight;
+        },
+        set : function(value) {
+            this.element.style.top = value - this.element.offsetHeight + "px";
+        }
+    },
+    right : {
+        get : function() {
+            return parseFloat(this.element.style.left) + this.element.offsetWidth || this.element.offsetWidth;
+        },
+        set : function(value) {
+            this.element.style.left = value - this.element.offsetWidth + "px";
+        }
+    }
+});
+
 var platforms = [];
-function Platform(x, y, width, height) {
+function createPlatform(x, y, width, height) {
     var div = document.createElement("div");
     div.className = "platform";
     document.body.appendChild(div);
-    this = new Sprite(div);
-    platforms.push(this);
-}
-
-function Sprite(element) {
-    Object.defineProperties(this, {
-        top : {
-            get : function() {
-                return parseFloat(element.style.top) || 0;
-            },
-            set : function(value) {
-                element.style.top = value + "px";
-            }
-        },
-        left : {
-            get : function() {
-                return parseFloat(element.style.left) || 0;
-            },
-            set : function(value) {
-                element.style.left = value + "px";
-            }
-        },
-        bottom : {
-            get : function() {
-                return parseFloat(element.style.top) + element.offsetHeight || element.offsetHeight;
-            },
-            set : function(value) {
-                element.style.top = value - element.offsetHeight + "px";
-            }
-        },
-        right : {
-            get : function() {
-                return parseFloat(element.style.left) + element.offsetWidth || element.offsetWidth;
-            },
-            set : function(value) {
-                element.style.left = value - element.offsetWidth + "px";
-            }
-        }
-    });
+    div.style.left = x;
+    div.style.top = y;
+    div.style.width = width;
+    div.style.height = height;
+    platforms.push(new Sprite(div));
 }
